0c082d00c9133ea49bfa56bafb3522bdbd8ca030
Wonseok <dannyhan8698@gmail.com>
2022-04-26 11:50:10 -0600

    Project start. Added GDI.

---
+++ ABulletHellGame/Form1.cs
@@ -0,0 +1,27 @@
+﻿/***********************************************************************************
+* Author: Wonseok Han
+* Date: 26/04/2022
+* Program: A Bullet Hell Game
+***********************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Diagnostics.Trace;
+using GDIDrawer;
+
+namespace ABulletHellGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
